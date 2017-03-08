e66bd3ea956a4991d8788ce3a803a8a6e0351523
JMoxey <jakemoxey@gmail.com>
2017-03-09 00:49:49 +1100

    feat: added image persistance (/:query/:giphyUrl)

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const { send } = require('micro');
 const microApi = require('micro-api');
 const fs = require('fs-promise')
 const request = require('request-promise').defaults({ encoding: null });
@@ -12,19 +13,31 @@ const handleNoSearchQuery = () => {
 
 const handleSearchGiphyUsingTranslate = async ({ params: { query }, res }) => {
   try {
+    // Lets search for the image with the query that's given...
     const resp = await request({ uri: GIPHY_TRANSLATE_API_URL, qs: { api_key: API_KEY, s: query }, json: true });
 
+    // Does such image even exist with this query??
     const imageUrl = getVal(resp, 'data.images.original.url');
     if (!imageUrl) {
       throw `No results for ${query}`;
     }
 
-    const gif = await request({ uri: imageUrl, json: true });
-    res.setHeader('Content-Type', 'image/gif');
-    return gif;
+    // If it does exist, then redirect the client (appending the image url to the path
+    // as we want to persist the image when the URL is shared to others).
+    res.setHeader('Location', `/${query}/${encodeURIComponent(imageUrl)}`);
+    return send(res, 301, {});
   } catch (e) {
     return e;
   }
+}
+
+const handleRequestGiphyImage = async ({ params: { giphyUrl }, res }) => {
+  // Request the image!
+  const gif = await request({ uri: decodeURIComponent(giphyUrl), json: true });
+
+  // Prepare the browser for an awesome GIF!
+  res.setHeader('Content-Type', 'image/gif');
+  return gif;
 }
 
 const api = microApi([
@@ -37,6 +50,11 @@ const api = microApi([
     method: 'get',
     path: '/:query',
     handler: handleSearchGiphyUsingTranslate,
+  },
+  {
+    method: 'get',
+    path: '/:query/:giphyUrl',
+    handler: handleRequestGiphyImage,
   }
 ]);
 
